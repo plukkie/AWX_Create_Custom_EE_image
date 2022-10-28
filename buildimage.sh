d07e09172a299145ab3ff285ed912e9c029c3356
Peter Plak <peter_plak@dell.com>
2022-10-28 11:58:41 +0200

    Initial upload

---
+++ buildimage.sh
@@ -0,0 +1,15 @@
+#DEFINE VARS
+image=<your repo name. i.e yourrepo/awx-ee:latest>
+
+
+python3 -m venv venv
+ansible-builder build --tag $image --context ./context --container-runtime docker
+
+# login to docker and push to docker hub
+docker login
+#docker push $image
+
+echo "Done"
+echo "Please Upload your image to docker hub: 'docker push $image'"
+echo && echo
+
